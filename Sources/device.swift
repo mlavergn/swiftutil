4e50fa48b19bcc7bc1e6847fbc8ee4a17fa41457
Marc Lavergne <mlavergn@gmail.com>
2017-04-21 12:48:10 -0400

    Enh: device add os lookup Enh: device add device name lookup

--- Sources/device.swift
+++ Sources/device.swift
@@ -11,10 +11,18 @@ import UIKit
 import IOKit
 #endif
 
+public enum DeviceOS: Int {
+	case iOS = 0
+	case macOS
+	case watchOS
+	case tvOS
+	case other
+}
+
 // MARK: - Device struct
 public struct Device {
 
-	/// Device unique identifier
+	/// Device unique identifier as a String
 	public static var deviceId: String {
 		Log.stamp()
 		var id: String = ""
@@ -31,5 +39,32 @@ public struct Device {
 		#endif
 
 		return id
+	}
+
+	/// Device name as a String
+	public static var name: String {
+		#if os(iOS)
+		return UIDevice.current.name
+		#else
+		guard let name = Host.current().localizedName else {
+			return ""
+		}
+		return name
+		#endif
+	}
+
+	/// OS as a DeviceOS enum
+	public static var os: DeviceOS {
+		#if os(iOS)
+			return .iOS
+		#elseif os(macOS)
+			return .macOS
+		#elseif os(watchOS)
+			return .watchOS
+		#elseif os(tvOS)
+			return .tvOS
+		#else
+			return .other
+		#endif
 	}
 }
